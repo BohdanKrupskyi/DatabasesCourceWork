1374b113cd605281a88b0f1141a500b3fae40c3e
BohdanKrupskyi <bkrupskyi@gmail.com>
2022-05-25 19:41:24 +0300

    seeding script for City and Department

---
+++ seed.sql
@@ -0,0 +1,16 @@
+﻿use LawFirmDb
+go
+
+insert into City (Name)
+values ('New York')
+     , ('Los Angeles')
+     , ('Chicago')
+     , ('Houston')
+go
+
+insert into Department (Name, CityId)
+values ('East Coast Lawyers', 1)
+     , ('LA Law', 2)
+     , ('North State Group', 3)
+     , ('Washington Law', 4)
+go
